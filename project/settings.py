64e6dc84e090233595dad4070b9b0822945e3c8b
Marek Brzóska <brzoskamarek@gmail.com>
2012-10-23 15:14:19 +0200

    set some jinja templates

--- project/settings.py
+++ project/settings.py
@@ -112,9 +112,13 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates', 'django'),
 )
 
+JINJA2_TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates', 'jinja2'),)
+
 INSTALLED_APPS = (
+    # django apps
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -122,9 +126,15 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    # external apps
+
+
+    # local apps
+
 )
 
 # A sample logging configuration. The only tangible logging
@@ -157,6 +167,6 @@ LOGGING = {
 }
 
 try:
-    execfile(os.path.join(PROJECT_ROOT, 'spa', 'settings_local.py'))
+    execfile(os.path.join(PROJECT_ROOT, 'project', 'settings_local.py'))
 except IOError:
     print "settings_local not found"
